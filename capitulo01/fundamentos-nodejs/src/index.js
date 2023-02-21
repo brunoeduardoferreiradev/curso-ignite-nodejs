8667537fe83a558522ca672f62913ad45ed6be8a
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-02-20 22:39:02 -0300

    Aula : Tipos de parâmetros

--- capitulo01/fundamentos-nodejs/src/index.js
+++ capitulo01/fundamentos-nodejs/src/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // instancia o express
 const app = express();
 
+// é um middleware que faz a conversão par json
+app.use(express.json());
+
 /*
  * GET - Buscar Informaçao dentro do servidor
  * POST - Inserir uma informaçao no servidor
@@ -12,18 +15,32 @@ const app = express();
  * DELETE - Deletar uma informação no servidor 
  */
 
+/**
+ * Tipos de Parâmetros
+ * 
+ * Route Params => identificar, editar, buscar ou deletar um recurso
+ * Query Params => Paginação / Filtro
+ * Body Params => Inserção ou Alteração de algum recurso ( JSON )
+ */
+
 // GET / courses
 app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json(["Curso 1", "Curso 2", "Curso 3"])
 })
 
 // POST /courses
 app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
 })
 
 // PUT /courses
 app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
 })
 
