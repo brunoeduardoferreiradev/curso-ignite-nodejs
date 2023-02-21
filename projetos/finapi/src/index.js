2ef00ac1db5ed513e0c11867aff0d64faa3ce88e
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-02-21 10:38:30 -0300

    Aula : Validando CPF existente

--- projetos/finapi/src/index.js
+++ projetos/finapi/src/index.js
@@ -7,19 +7,27 @@ app.use(express.json());
 
 const customers = [];
 
-
+// POST /account - Cria uma conta
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+
+  // Procura dentro de customers pra ver se ja existe um cpf
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf);
+
+  // Se o cpf ja existir retorna um erro 400 e uma mensagem de erro  
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists" })
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   });
 
-  return response.status(201).send();
+  return response.status(201).json({ message: `ID : ${customers[0].id}` });
 
 })
 
