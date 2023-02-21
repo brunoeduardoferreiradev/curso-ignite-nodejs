b903e206f55153695aa7fb0f4ee2e406563340a5
Bruno Eduardo de Moura Ferreira <125761885+brunoeduardoferreiradev@users.noreply.github.com>
2023-02-20 22:08:30 -0300

    Aula : Utilizando os métodos HTTP

--- capitulo01/fundamentos-nodejs/src/index.js
+++ capitulo01/fundamentos-nodejs/src/index.js
@@ -4,8 +4,37 @@ const express = require('express');
 // instancia o express
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World Ignite - Fundamentos NodeJS!" })
+/*
+ * GET - Buscar Informaçao dentro do servidor
+ * POST - Inserir uma informaçao no servidor
+ * PUT - Alterar uma informaçao no servidor
+ * PATCH - Alterar uma informação específica no servidor
+ * DELETE - Deletar uma informação no servidor 
+ */
+
+// GET / courses
+app.get("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"])
+})
+
+// POST /courses
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+// PUT /courses
+app.post("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+// PATCH /courses
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"])
+})
+
+// DELETE / courses
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 4"])
 })
 
 // porta onde a aplicação será inicializada
